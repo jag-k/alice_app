0b39664333459f24f7b821c2c6e71559f5138872
Jag_k <admin@jag-k.info>
2019-04-20 14:48:08 +0300

    Update project

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request
-import logging
 from alice_sdk import *
 from main_fuction import *
 
 app = Flask(__name__)
 session_storage = {}
-logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/')
@@ -17,12 +15,9 @@ def hello_world():
 def main():
     alice_request = AliceRequest(request.json)
     alice_response = AliceResponse(alice_request)
-    logging.info("Request {}".format(alice_request))
     alice_response, session_storage[alice_request.user_id] = handle_dialog(
         alice_request, alice_response, session_storage.get(alice_request.user_id)
     )
-    logging.info(("Response {}".format(alice_response)))
-
     return alice_response.dumps()
 
 

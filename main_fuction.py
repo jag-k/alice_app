c90ba5a1a7c6eb807b508d064203637e3176516e
Jag_k <admin@jag-k.info>
2019-04-20 14:41:23 +0300

    Update project

--- main_fuction.py
+++ main_fuction.py
@@ -2,5 +2,7 @@ from little_functions import *
 
 
 def handle_dialog(request, response, user_storage):
+    if not user_storage:
+        user_storage = {}
     output_message = "Hello, world."
     return message_return(response, user_storage, output_message)

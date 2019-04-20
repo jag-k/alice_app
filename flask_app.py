530cc429eb3cba913b6c6fdc53f071ce851add49
Jag_k <admin@jag-k.info>
2019-04-20 13:55:17 +0300

    Update project

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, request
+import logging
+from alice_sdk import *
+from main_fuction import *
 
 app = Flask(__name__)
+session_storage = {}
+logging.basicConfig(level=logging.DEBUG)
 
 
 @app.route('/')
@@ -8,5 +13,21 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/", methods=["POST"])
+def main():
+    alice_request = AliceRequest(request.json)
+    alice_response = AliceResponse(alice_request)
+    logging.info("Request {}".format(alice_request))
+
+    alice_response, session_storage[alice_request.user_id] = handle_dialog(
+        alice_request, alice_response, session_storage.get(alice_request.user_id)
+    )
+
+    logging.info("Response {}".format(alice_response))
+
+    return alice_response.dumps()
+
+
 if __name__ == '__main__':
     app.run()
+
---
+++ little_functions.py
@@ -0,0 +1,30 @@
+import requests, random, ujson
+
+
+def get_suggests(user_storage):
+    if "suggests" in user_storage.keys():
+        suggests = []
+        for suggest in user_storage["suggests"]:
+            suggests.append({"title": suggest, "hide": True})
+    else:
+        suggests = []
+
+    return suggests, user_storage
+
+
+def read_data(name: str) -> dict:
+    return ujson.load(open(name + ".json", encoding="utf-8"))
+
+
+def message_return(response, user_storage, message):
+    response.set_text(message)
+    response.set_tts(message)
+    buttons, user_storage = get_suggests(user_storage)
+    response.set_buttons(buttons)
+    return response, user_storage
+
+
+def message_error(response, user_storage):
+    message = random.choice(["Я тебя не поняла, повтори, пожалуйста.", "Не очень хорошо поняла, ответь правильно."])
+    return message_return(response, user_storage, message)
+

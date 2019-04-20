a6a72685df6af9d6083d1aee29b9b1fbee6c7860
Jag_k <admin@jag-k.info>
2019-04-20 14:45:53 +0300

    Update project

--- flask_app.py
+++ flask_app.py
@@ -17,10 +17,11 @@ def hello_world():
 def main():
     alice_request = AliceRequest(request.json)
     alice_response = AliceResponse(alice_request)
-
+    logging.info("Request {}".format(alice_request))
     alice_response, session_storage[alice_request.user_id] = handle_dialog(
         alice_request, alice_response, session_storage.get(alice_request.user_id)
     )
+    logging.info(("Response {}".format(alice_response)))
 
     return alice_response.dumps()
 
